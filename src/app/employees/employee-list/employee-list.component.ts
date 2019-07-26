4c1656a40865e3ba278787b030d88d8333b1d952
Juan Aleman <juan.aleman.c@gmail.com>
2019-07-26 13:56:37 -0600

    Agregar funcionalidad al cuadro de búsqueda.

--- src/app/employees/employee-list/employee-list.component.ts
+++ src/app/employees/employee-list/employee-list.component.ts
@@ -32,5 +32,11 @@ export class EmployeeListComponent implements OnInit {
       }
     );
   }
-
+onSearchClear() {
+  this.searchKey = '';
+  this.applyFilter();
+}
+applyFilter() {
+  this.listData.filter = this.searchKey.trim().toLocaleLowerCase();
+}
 }
