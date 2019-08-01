eed8e3fe0d1d98f86b0eb583a9677246892602b4
Juan Aleman <juan.aleman.c@gmail.com>
2019-08-01 12:37:46 -0600

    MatDialog funcionalidad biton cerrar

--- src/app/mat-confirm-dialog/mat-confirm-dialog.component.ts
+++ src/app/mat-confirm-dialog/mat-confirm-dialog.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Inject } from '@angular/core';
-import { MAT_DIALOG_DATA } from '@angular/material';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material';
 
 @Component({
   selector: 'app-mat-confirm-dialog',
@@ -8,9 +8,13 @@ import { MAT_DIALOG_DATA } from '@angular/material';
 })
 export class MatConfirmDialogComponent implements OnInit {
 
-  constructor(@Inject(MAT_DIALOG_DATA) public data) { }
+  constructor(@Inject(MAT_DIALOG_DATA) public data,
+              public dialogRef: MatDialogRef<MatConfirmDialogComponent>) { }
 
   ngOnInit() {
+  }
+  closeDialog() {
+    this.dialogRef.close();
   }
 
 }
